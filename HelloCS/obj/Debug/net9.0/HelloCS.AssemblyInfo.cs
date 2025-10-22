7a0a98d1da78bdb9274b83d046c975bc68737bc1
hashimcoder <hashimcoder7@gmail.com>
2025-10-22 09:02:36 +0530

    first commit

--- HelloCS/obj/Debug/net9.0/HelloCS.AssemblyInfo.cs
+++ HelloCS/obj/Debug/net9.0/HelloCS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83afcd566f26088036053bf2c191d2f8148cfaa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
